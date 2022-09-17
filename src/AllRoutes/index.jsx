0b51c65ff24716c16e45b31941ab4a64300dae4d
thuta_yoma <thutasann@yomabank.com>
2022-09-17 14:09:37 +0630

    routes set up

---
+++ src/AllRoutes/index.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom';
+import Footer from '../Components/Footer';
+import NavBar from '../Components/Navbar';
+import Home from '../Pages/Home';
+import Reviews from '../Pages/Reviews';
+import About from '../Pages/About';
+import FAQ from '../Pages/FAQ';
+import Help from '../Pages/Help';
+import Products from '../Pages/Products';
+import PgaeNotFound from '../Pages/404';
+
+const AllROutes = () => {
+    return (
+        <>
+            <NavBar/>
+                <Routes>
+                    <Route path="/" element={<Home/>} />
+                    <Route path="/reviews" element={<Reviews/>} />
+                    <Route path="/about" element={<About/>} />
+                    <Route path="/faq" element={<FAQ/>} />
+                    <Route path="/help" element={<Help/>} />
+                    <Route path="/:category" element={<Products/>} />
+                    <Route path="/*" element={<PgaeNotFound/>} />
+                </Routes>   
+            <Footer/>
+        </>
+    )
+}
+
+export default AllROutes
